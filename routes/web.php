c11fc48659c9f74bfe9e68133da1043a5397fab5
Kieren <kieren.pinto@hotmail.com>
2023-06-03 16:25:10 +1000

    Install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,19 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/home', function () {
-  return (new \App\Http\Controllers\HomeController())->index();
-  // return \App\Http\Controllers\HomeController::class->index();
-    // return view('index');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::view('/contact', 'contact')->name('contact'); // Shortcut view syntax
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
