a9ee8a7a1bbbf29920df351cfc495e10e34ddfa8
Kieren <kieren.pinto@hotmail.com>
2024-08-06 22:33:00 +1000

    Create test endpoints in the HomeController and routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,18 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Group;
+use Illuminate\Container\Container;
 
 class HomeController extends Controller
 {
-    function index()
+    public function index()
     {
         $allCategories = ['Category 1', 'Category 2'];
         dd("Test HomeController Index Web Endpoint. This hasn't been implemented yet. - KP");
     }
 
-    function php_info()
+    public function php_info()
     {
         phpinfo();
+    }
+
+    public function testGroup(Group $group, Container $container)
+    {
+        // dd($container);
+        dd($group);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', [HomeController::class, 'index'])->name('home.index');
+Route::get('/php_info', [HomeController::class, 'php_info'])->name('home.php_info');
+Route::get('/testGroup', [HomeController::class, 'testGroup'])->name('home.testGroup');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,8 +31,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/home', [HomeController::class, 'index'])->name('home.index');
-    Route::get('/php_info', [HomeController::class, 'php_info'])->name('home.index');
     Route::get('/groups', [GroupController::class, 'index'])->name('group.all');
     Route::get('/groups/{group}', [GroupController::class, 'edit'])->name('group.edit');
     Route::patch('/groups/{group}', [GroupController::class, 'update'])->name('group.update');
